0f5ad0ad36358317fc50afc4afbffa566c1fde79
Z.R.F <bb.zrf@163.com>
2023-07-26 23:34:36 +0800

    Marble和Battle场景切换逻辑

--- Assets/HotUpdate/Scripts/Common/CameraDirector.cs
+++ Assets/HotUpdate/Scripts/Common/CameraDirector.cs
@@ -1,30 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
+using Lean.Touch;
 
 public class CameraDirector : MonoBehaviour
 {
+    // LeanTouch
+    public LeanFingerFilter Use = new LeanFingerFilter(true);
 
-    private float mDirectorFactor = 1.0f;
-    public float DirectorFactor
-    {
-        get { return mDirectorFactor; }
-        set { mDirectorFactor = value; }
-    }
+    [Range(0.1f, 10f)]
+	public float _Sensitivity = 1.0f;
+
+	private float mDirectorFactor = 0.0f;
+    private float mTimelineDuration = 0.0f;
+    private PlayableDirector mDirector = null;
+    
 
     void Start()
     {
-        Lean.Touch.LeanTouch.OnFingerSwipe += OnFingerSwipe;
-    }
-
-    private void OnFingerSwipe(Lean.Touch.LeanFinger obj)
-    {
-        
+        mDirector = GetComponent<PlayableDirector>();
+        mTimelineDuration = (float)mDirector.duration;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+	protected virtual void LateUpdate()
+	{
+		// Get the fingers we want to use
+		var fingers = Use.UpdateAndGetFingers();
+
+		float screenDelta = LeanGesture.GetScreenDelta(fingers).y * _Sensitivity * 0.001f;
+        mDirectorFactor += -screenDelta;
+        mDirectorFactor = Mathf.Clamp01(mDirectorFactor);
+
+        mDirector.time = mDirectorFactor * mTimelineDuration;
+        mDirector.Evaluate();
     }
 }
